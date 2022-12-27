d02166ddeb5e881beca2af5bfb310ca769cf1d88
Mohit Arora <mhiaror@amazon.com>
2022-12-27 11:48:15 -0500

    wrap csm api usage in try catch block

--- src/service/csmService.js
+++ src/service/csmService.js
@@ -31,36 +31,41 @@ class CsmService {
         } catch (error) {
             this.logger.error("Load csm script error: ", error);
         }
-    };
+    }
   
     initializeCSM() {
     // avoid multiple initialization
-        if (this.csmInitialized) {
-            return;
-        }
-        const region = GlobalConfig.getRegion();
-        const csmWorkerText = csmWorkerString.replace(/\\/g, '');
-        const sharedWorkerBlobUrl = URL.createObjectURL(new Blob([csmWorkerText], { type: 'text/javascript' }));
-        const ldasEndpoint = getLdasEndpointUrl(region);
-        let params = {
-            endpoint: ldasEndpoint,
-            namespace: CHAT_WIDGET_METRIC_NAME_SPACE,
-            sharedWorkerUrl: sharedWorkerBlobUrl,
-        };
-
-        csm.initCSM(params);
-        this.logger.info(`CSMService is initialized in ${region}`);
-        this.csmInitialized = true;
-        if (this.metricsToBePublished) {
-            this.metricsToBePublished.forEach((metric) => {
-                csm.API.addMetric(metric);
-            });
-            this.metricsToBePublished = null;
+        try {
+            if (this.csmInitialized) {
+                return;
+            }
+            const region = GlobalConfig.getRegion();
+            const csmWorkerText = csmWorkerString.replace(/\\/g, '');
+            const sharedWorkerBlobUrl = URL.createObjectURL(new Blob([csmWorkerText], { type: 'text/javascript' }));
+            const ldasEndpoint = getLdasEndpointUrl(region);
+            let params = {
+                endpoint: ldasEndpoint,
+                namespace: CHAT_WIDGET_METRIC_NAME_SPACE,
+                sharedWorkerUrl: sharedWorkerBlobUrl,
+            };
+    
+            csm.initCSM(params);
+            this.logger.info(`CSMService is initialized in ${region}`);
+            this.csmInitialized = true;
+            if (this.metricsToBePublished) {
+                this.metricsToBePublished.forEach((metric) => {
+                    csm.API.addMetric(metric);
+                });
+                this.metricsToBePublished = null;
+            }
+        } catch(err) {
+            this.logger.error('Failed to initialize csm: ', err);
         }
     }
 
     updateCsmConfig(csmConfig) {
-        this.widgetType = csmConfig.widgetType || this.widgetType;
+        this.widgetType = typeof csmConfig === "object" && csmConfig !== null && !Array.isArray(csmConfig) ?
+                            csmConfig.widgetType : this.widgetType;
     }
 
     getDefaultDimensions() {
@@ -73,14 +78,18 @@ class CsmService {
     }
 
     addMetric(metric) {
-    // if csmService is never initialized, store the metrics in an array
+        // if csmService is never initialized, store the metrics in an array
         if (!this.csmInitialized) {
             if (this.metricsToBePublished) {
                 this.metricsToBePublished.push(metric);
                 this.logger.info(`CSMService is not initialized yet. Adding metrics to queue to be published once CSMService is initialized`);
             }
         } else {
-            csm.API.addMetric(metric);
+            try {
+                csm.API.addMetric(metric);
+            } catch(err) {
+                this.logger.error('Failed to addMetric csm: ', err);
+            }
         }
     }
 
@@ -91,27 +100,31 @@ class CsmService {
     }
 
     addLatencyMetric(method, timeDifference, category, otherDimensions = []) {
-        const latencyMetric = new csm.Metric(
-            method,
-            csm.UNIT.MILLISECONDS,
-            timeDifference
-        );
-        const dimensions = [
-            ...this.getDefaultDimensions(),
-            {
-                name: "Metric",
-                value: "Latency",
-            },
-            {
-                name: DIMENSION_CATEGORY,
-                value: category
-            },
-            ...otherDimensions
-        ];
-        this.setDimensions(latencyMetric, dimensions);
-        this.addMetric(latencyMetric);
-        this.logger.debug(`Successfully published latency API metrics for method ${method}`);
-    };
+        try {
+            const latencyMetric = new csm.Metric(
+                method,
+                csm.UNIT.MILLISECONDS,
+                timeDifference
+            );
+            const dimensions = [
+                ...this.getDefaultDimensions(),
+                {
+                    name: "Metric",
+                    value: "Latency",
+                },
+                {
+                    name: DIMENSION_CATEGORY,
+                    value: category
+                },
+                ...otherDimensions
+            ];
+            this.setDimensions(latencyMetric, dimensions);
+            this.addMetric(latencyMetric);
+            this.logger.debug(`Successfully published latency API metrics for method ${method}`);
+        } catch (err) {
+            this.logger.error('Failed to addLatencyMetric csm: ', err);
+        }
+    }
 
     addLatencyMetricWithStartTime(method, startTime, category, otherDimensions = []) {
         const endTime = new Date().getTime();
@@ -121,81 +134,93 @@ class CsmService {
     }
 
     addCountAndErrorMetric(method, category, error, otherDimensions = []) {
-        const dimensions = [
-            ...this.getDefaultDimensions(),
-            {
-                name: DIMENSION_CATEGORY,
-                value: category
-            },
-            ...otherDimensions
-        ];
-        const countMetric = new csm.Metric(method, csm.UNIT.COUNT, 1);
-        this.setDimensions(countMetric, [
-            ...dimensions,
-            {
-                name: "Metric",
-                value: "Count",
-            }
-        ]);
-        const errorCount = error ? 1 : 0;
-        const errorMetric = new csm.Metric(method, csm.UNIT.COUNT, errorCount);
-        this.setDimensions(errorMetric, [
-            ...dimensions,
-            {
-                name: "Metric",
-                value: "Error",
-            }
-        ]);
-        this.addMetric(countMetric);
-        this.addMetric(errorMetric);
-        this.logger.debug(`Successfully published count and error metrics for method ${method}`);
-    };
+        try {
+            const dimensions = [
+                ...this.getDefaultDimensions(),
+                {
+                    name: DIMENSION_CATEGORY,
+                    value: category
+                },
+                ...otherDimensions
+            ];
+            const countMetric = new csm.Metric(method, csm.UNIT.COUNT, 1);
+            this.setDimensions(countMetric, [
+                ...dimensions,
+                {
+                    name: "Metric",
+                    value: "Count",
+                }
+            ]);
+            const errorCount = error ? 1 : 0;
+            const errorMetric = new csm.Metric(method, csm.UNIT.COUNT, errorCount);
+            this.setDimensions(errorMetric, [
+                ...dimensions,
+                {
+                    name: "Metric",
+                    value: "Error",
+                }
+            ]);
+            this.addMetric(countMetric);
+            this.addMetric(errorMetric);
+            this.logger.debug(`Successfully published count and error metrics for method ${method}`);
+        } catch(err) {
+            this.logger.error('Failed to addCountAndErrorMetric csm: ', err);
+        }
+    }
 
     addCountMetric(method, category, otherDimensions = []) {
-        const dimensions = [
-            ...this.getDefaultDimensions(),
-            {
-                name: DIMENSION_CATEGORY,
-                value: category
-            },
-            {
-                name: "Metric",
-                value: "Count",
-            },
-            ...otherDimensions
-        ];
-        const countMetric = new csm.Metric(method, csm.UNIT.COUNT, 1);
-        this.setDimensions(countMetric, dimensions);
-        this.addMetric(countMetric);
-        this.logger.debug(`Successfully published count metrics for method ${method}`);
+        try {
+            const dimensions = [
+                ...this.getDefaultDimensions(),
+                {
+                    name: DIMENSION_CATEGORY,
+                    value: category
+                },
+                {
+                    name: "Metric",
+                    value: "Count",
+                },
+                ...otherDimensions
+            ];
+            const countMetric = new csm.Metric(method, csm.UNIT.COUNT, 1);
+            this.setDimensions(countMetric, dimensions);
+            this.addMetric(countMetric);
+            this.logger.debug(`Successfully published count metrics for method ${method}`);
+        } catch(err) {
+            this.logger.error('Failed to addCountMetric csm: ', err);
+        }
     }
 
     addAgentCountMetric(metricName, count) {
-        const _self = this;
-        if (csm?.API?.addCount && metricName) {
-            csm.API.addCount(metricName, count);
-            _self.MAX_RETRY = 5;
-        } else {
-            //add to list and retry later
-            if (metricName) {
-                this.agentMetricToBePublished.push({
-   
-                    metricName,
-   
-                    count
-                });
-            }
-            setTimeout(() => {
-                if (csm?.API?.addCount) {
-                    this.agentMetricToBePublished.forEach(metricItem => {
-                        csm.API.addCount(metricItem.metricName, metricItem.count);
+        try {
+            const _self = this;
+            if (csm && csm.API.addCount && metricName) {
+                csm.API.addCount(metricName, count);
+                _self.MAX_RETRY = 5;
+            } else {
+                //add to list and retry later
+                if (metricName) {
+                    this.agentMetricToBePublished.push({
+    
+                        metricName,
+    
+                        count
                     });
-                    this.agentMetricToBePublished = [];
-                } else if(_self.MAX_RETRY > 0) {
-                    _self.MAX_RETRY -= 1;
-                    _self.addAgentCountMetric();
                 }
-            }, 3000);
+                setTimeout(() => {
+                    if (csm && csm.API.addCount) {
+                        this.agentMetricToBePublished.forEach(metricItem => {
+                            csm.API.addCount(metricItem.metricName, metricItem.count);
+                        });
+                        this.agentMetricToBePublished = [];
+                    } else if(_self.MAX_RETRY > 0) {
+                        _self.MAX_RETRY -= 1;
+                        _self.addAgentCountMetric();
+                    }
+                }, 3000);
+            }
+        } catch(err) {
+            this.logger.error('Failed to addAgentCountMetric csm: ', err);
         }
     }
 }
