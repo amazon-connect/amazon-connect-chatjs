7d8f0329c97893a7f22cf1855243ff569be74d72
mrajatttt <143978428+mrajatttt@users.noreply.github.com>
2024-07-23 16:10:28 -0700

    fixing error loggin caused by ChatJS (#226)
    * fixing Streams SDK bug caused by ChatJS
    
    * Bumped chatjs version

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ import { LogManager, LogLevel } from "./log";
 var global = typeof global !== 'undefined' ? global :
     typeof self !== 'undefined' ? self :
         typeof window !== 'undefined' ? window : {};
+/**
+ * connect is a shared namespace used by Streams SDK,
+ * so any property added to connect namespace overrides
+ * Streams SDK. Since Streams is initialized before ChatJS,
+ * adding optionals here so Streams SDK namespace is not overridden.
+ *  */
 global.connect = global.connect || {};
-connect.ChatSession = ChatSessionObject;
-connect.LogManager = LogManager;
-connect.LogLevel = LogLevel;
-connect.csmService = ChatSessionObject.csmService;
+connect.ChatSession = connect.ChatSession || ChatSessionObject;
+connect.LogManager = connect.LogManager || LogManager;
+connect.LogLevel = connect.LogLevel || LogLevel;
+connect.csmService = connect.csmService || ChatSessionObject.csmService;
 export const ChatSession = ChatSessionObject;
