d05f3325a3e94cbcce52cc6bbfb4af8c058d845b
spenlep-amzn <150714337+spenlep-amzn@users.noreply.github.com>
2025-05-29 15:16:40 -0700

    remove files

--- src/chat.js
+++
@@ -1,8 +0,0 @@
-import { CHAT_CONFIGURATIONS } from './constants';
-
-//Placeholder
-export default class Chat {
-    static getActiveChats() {
-        return CHAT_CONFIGURATIONS.CONCURRENT_CHATS;
-    }
-}
--- src/config/csmConfig.js
+++
@@ -1,6 +0,0 @@
-export const CHAT_WIDGET_METRIC_NAME_SPACE = "chat-widget";
-export const DEFAULT_WIDGET_TYPE = "CustomChatWidget";
-
-export const getLdasEndpointUrl = (region) => {
-    return `https://ieluqbvv.telemetry.connect.${region}.amazonaws.com/prod`;
-};
